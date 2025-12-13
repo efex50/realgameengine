da5a132613ec1e1e5340087c860c1dd864d6f275
efex50 <aefex50@gmail.com>
2025-12-13 14:14:12 +0300

    up

---
+++ examples/test_example/web/pkg/test_example_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: () => void;
+export const send_message: (a: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h8f410cedb33a71c6: (a: number, b: number) => void;
+export const wasm_bindgen__closure__destroy__heb27498e19e481e4: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
