4bb0935a1c282e04fb6e430c6ab7c97145e918bf
aefe <aefex50@gmail.com>
2026-02-28 11:02:46 +0300

    System first upgrade

--- examples/test_example/src/lib.rs
+++ examples/test_example/src/lib.rs
@@ -1,27 +1,9 @@
-use realgameengine::{prelude::*, thread::{init_global_pool, spawn_global}, thread_pool::{self}};
+use realgameengine::prelude::*;
 
 #[main]
 fn main(){
-    
-    init_global_pool(4);
-    let g = Engine::new("zort");
-    g.game_loop();
-
-
-
-
-}
-
-
-#[cfg(not(target_family = "wasm"))]
-#[test]
-fn onehsot_test(){
-    let handle = spawn_global(||{
-        println!("thread'dan salamlar 2sn lütfen");
-        std::thread::sleep(std::time::Duration::from_secs(2));
-        return "sa";
-    });
-    println!("started the threads");
-    let hres = handle.join();
-    println!("thread ended :{:?}",hres);
+    let w = Engine::new("hello");
+    global_info("sa");
+    w.game_loop();
+    if cfg!(target_os = "windows"){}
 }
